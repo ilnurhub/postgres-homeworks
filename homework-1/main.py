c07b3d82a49c4b6fe2e2415bf97757a55c2e8468
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-29 23:40:00 +0300

    add def create_file_path()

--- homework-1/main.py
+++ homework-1/main.py
@@ -4,3 +4,11 @@ import os
 import csv
 
 DIRNAME = 'north_data'
+
+
+def create_file_path(filename):
+    """
+    Создает относительный путь до заданного файла из папки homework-1
+    """
+    f_path = os.path.join(DIRNAME, filename)
+    return f_path
