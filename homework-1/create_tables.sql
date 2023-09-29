b7f5f4f701077aaab9115e292b5b6034b8192792
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-29 22:08:58 +0300

    create table employees

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,11 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(30) NOT NULL,
+	last_name varchar(30) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
