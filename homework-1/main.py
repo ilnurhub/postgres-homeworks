ec0ac65cf9aa62ba393c5a2700c60d9c9b95dd0a
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-29 23:59:44 +0300

    fill table customers

--- homework-1/main.py
+++ homework-1/main.py
@@ -12,3 +12,18 @@ def create_file_path(filename):
     """
     f_path = os.path.join(DIRNAME, filename)
     return f_path
+
+
+customers_file_path = create_file_path('customers_data.csv')
+employees_file_path = create_file_path('employees_data.csv')
+orders_file_path = create_file_path('orders_data.csv')
+
+with psycopg2.connect(host='localhost', database='north', user='postgres', password='GoldSa1mon!') as conn:
+    with conn.cursor() as cur:
+        with open(customers_file_path, 'r', encoding='utf-8') as file:
+            data = csv.DictReader(file)
+            for customer_data in data:
+                cur.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                            (customer_data['customer_id'], customer_data['company_name'], customer_data['contact_name']))
+
+conn.close()
