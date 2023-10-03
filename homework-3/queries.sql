894e81286dfd8e9c68a80c72f5654d84b4db7b85
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-10-03 14:46:57 +0300

    completed task_3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -20,7 +20,9 @@ WHERE discontinued = 0
 ORDER BY units_in_stock;
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name
+FROM customers
+WHERE NOT EXISTS(SELECT * FROM orders WHERE customers.customer_id = orders.customer_id);
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
